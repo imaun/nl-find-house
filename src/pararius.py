1af3cb15120a2515f5a012882f5981c111585935
imaun <imun22@gmail.com>
2023-10-14 21:08:35 +0200

    pararius get price & title & address

--- src/pararius.py
+++ src/pararius.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.pararius.com/apartments/amsterdam'
+base_url = 'https://www.pararius.com'
 
 page = requests.get(url)
 html = BeautifulSoup(page.content, 'html.parser')
@@ -9,7 +10,19 @@ html = BeautifulSoup(page.content, 'html.parser')
 search_result = html.find('ul', {"class": "search-list"})
 items = search_result.find_all("li", {"class": "search-list__item"})
 print(len(items))
-for res in items:
-    print(res, end="\n"*2)
+for item in items:
+    # print(res, end="\n"*2)
+    section = item.find('section')
+    e_title = item.find('h2', {"class": "listing-search-item__title"})
+    title = e_title.text.strip()
+    print(title)
+    title_href = e_title.find('a').get('href')
+    print(base_url + title_href)
+    address = item.find('div', {"class": "listing-search-item__sub-title'"}).text.strip()
+    print(address)
+    price_text = item.find("div", {"class": "listing-search-item__price"}).text.strip()
+    price = int(price_text[1:].split(' ')[0].replace(',', ''))
+    print(price)
+    exit(0)
 
 print('end')
