65ca23421a93753d9a2828b52353b6c826951056
imaun <imun22@gmail.com>
2024-05-22 23:01:57 +0200

    use render_template in email_client

--- src/email_client.py
+++ src/email_client.py
@@ -3,6 +3,7 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from models import House
+import template_render
 
 email_account = os.getenv('NLH_EMAIL_ACCOUNT')
 email_password = os.getenv('NLH_EMAIL_PASSWORD')
@@ -10,34 +11,16 @@ email_server = os.getenv('NLH_EMAIL_SERVER')
 email_port = os.getenv('NLH_EMAIL_PORT')
 
 
-def read_text():
-    file_path = 'data/email_template.txt'
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f'The text template for sending email at {file_path} not found')
-
-    with open(file_path, 'r') as file:
-        template = file.read()
-
-
-def replace_params(house: House, template: str):
-    template = template.replace('{title}', house.title)
-    template = template.replace('{source}', house.source_name)
-    template = template.replace('{url}', house.url)
-    template = template.replace('{city}', house.city)
-    template = template.replace('{price}', house.price_text)
-    template = template.replace('{postal_code}', house.postal_code)
-    template = template.replace('{description}', house.description)
-
-    return template
-
 def send(house: House, target_email: str):
     message = MIMEMultipart('alternative')
     message['Subject'] = f'{house.title}'
     message['From'] = email_account
     message['To'] = target_email
+    text_body = template_render.render_template(house, mode='text')
+    html_body = template_render.render_template(house, mode='html')
+    text_email = MIMEText(text_body, 'plain')
+    text_body = MIMEMultipart(html_body, 'html')
 
-    text_template = read_text()
-    body = replace_params(house, text_template)
 
 
 
